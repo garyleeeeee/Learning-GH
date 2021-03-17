4c0d6ca8a4a1d260ed7ccee1bc6dcebd953b74bb
Gaz <garyleeeeee@gmail.com>
2021-03-17 13:30:52 +0800

    Initial Commit

---
+++ Learning GitHUb/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Learning GitHUb
+//
+//  Created by GUOLI LI on 17/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Learning GitHUb/Learning_GitHUbApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_GitHUbApp.swift
+//  Learning GitHUb
+//
+//  Created by GUOLI LI on 17/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct Learning_GitHUbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
